455b9ee0a37bb1200fc1370106733be77cbb69e5
Sreedevi <sreedevibiju5@gmail.com>
2024-04-30 14:01:13 +0530

    30

--- src/com/allianz/alertdemo/Demo1.java
+++ src/com/allianz/alertdemo/Demo1.java
@@ -11,23 +11,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Demo1 {
 
 	public static void main(String[] args) {
+		
 		WebDriver driver=new ChromeDriver();
-	    driver.manage().window().maximize();
-	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-	        
-	    driver.get("https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm");
-	    
-	    //f12 or ctrl+sht+c
-	    
-	    driver.findElement(By.xpath("//img[@alt='Go']")).click();
-	    
-	    //handle alert
-	    WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
-	    wait.until(ExpectedConditions.alertIsPresent());
-	    String actualAlertText=driver.switchTo().alert().getText();
-	    System.out.println(actualAlertText);
-	    
-	    driver.switchTo().alert().accept();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm");
+		
+		//to inspect --> f12 or ctrl+shift+c
+		//click on Go
+		//driver.findElement(By.xpath("//img[@alt='Go']")).click();
+		
+		//wait for alert present
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+		String actualAlertText=driver.switchTo().alert().getText();
+		System.out.println(actualAlertText);
+		
+		driver.switchTo().alert().accept();
+		
+
 	}
 
 }
